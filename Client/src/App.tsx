6f65fd5db40c111169bc29a4991cbf97e8dd5498
Tal Maman <dreapet41s@gmail.com>
2025-07-08 19:19:53 +0300

    added shadcn library added themeprovidor and toggles

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,8 +1,12 @@
+import { ThemeProvider } from "./components/ui/theme-provider"
+import { ModeToggle } from "./components/ui/mode-toggle"
 function App() {
-
   return (
     <>
-      <h1 className='text-3xlfont-bold underline'>Hello World</h1>
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <ModeToggle />
+        <h1 className='text-3xlfont-bold underline'>Hello World</h1>
+      </ThemeProvider>
     </>
   )
 }
